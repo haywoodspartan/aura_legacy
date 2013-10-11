f263b9fb61a508f2efda6bb7e282c612a8929103
Fuhhue <hallbrighton@hotmail.com>
2013-10-11 13:52:32 -0400

    Added Hide Skill Handler

---
+++ src/WorldServer/Skills/Handlers/Actions/HideHandler.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Aura development team - Licensed under GNU GPL
+// Coding By Fuhhue
+using Aura.Shared.Const;
+using Aura.World.Network;
+using Aura.World.World;
+namespace Aura.World.Skills
+{
+        [SkillAttr(SkillConst.Hide)]
+public class HideHandler : StartStopSkillHandler
+{
+  public override SkillResults Start(MabiCreature creature, MabiSkill skill)
+  {
+                        creature.Activate(CreatureConditionB.Transparent);
+                       
+                        Send.StatusEffectUpdate(creature);
+   return SkillResults.Okay;
+  }
+  public override SkillResults Stop(MabiCreature creature, MabiSkill skill)
+  {
+                        creature.Deactivate(CreatureConditionB.Transparent);
+                       
+                        Send.StatusEffectUpdate(creature);
+   return SkillResults.Okay;
+  }
+}
+}
